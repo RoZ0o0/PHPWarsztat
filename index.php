3314dbd43fc3d5dcba78750dc2a85e119dc42a06
cyziek <krzysztof112997@gmail.com>
2021-04-22 13:16:04 +0100

    dodano stronę logowania

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<meta charset="utf-8">
+<html lang="pl">
+<head>
+
+
+<link rel="stylesheet" href="./css/bootstrap.min.css">
+<script src="./js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="style.css">
+
+  <title>Warsztaty Blacharskie</title>
+
+ 
+</head>
+<body>
+<div class="jumbotron text-center">
+ <h1>Obsługa warsztatów blacharskich</h1>
+ <p>adadad</p>
+ </div>
+<form class="box" action="login.php" method="post" >
+
+<h1> Logowanie </h1>
+<div id="inputs">
+<br><input type="text" name="login" placeholder="Login"/> <br>
+<br><input type="password" name="password" placeholder="Hasło"/><br>
+</div>
+<button type="submit" class="btn btn-outline-primary btn-lg" value="Zaloguj">Zaloguj</button>
+</form>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<meta charset="utf-8">
+<html lang="pl">
+<head>
+
+<title>Warsztaty Blacharskie</title>
+
+<link rel="stylesheet" href="./css/bootstrap.min.css">
+<script src="./js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="style.css">
+
+
+</head>
+
+<body>
+<h2>Witaj <?php echo htmlspecialchars($_POST['login']); ?>.
+</h2>
+</body>
+
+
+</html>
+
+
