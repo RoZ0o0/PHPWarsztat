e5bef0bb516bc6af724385d9ac3cc3d2f6e8ae21
cyziek <krzysztof112997@gmail.com>
2021-04-24 18:06:01 +0100

    zmieniona szata graficzna logowania i rejestracji

--- index.php
+++ index.php
@@ -4,28 +4,59 @@
 <head>
 
 
-<link rel="stylesheet" href="./css/bootstrap.min.css">
-<script src="./js/bootstrap.min.js"></script>
+<!-- <link rel="stylesheet" href="./css/bootstrap.min.css">
+<script src="./js/bootstrap.min.js"></script> Tymczasowo wyłączone-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
 
   <title>Warsztaty Blacharskie</title>
 
  
 </head>
 <body>
-<div class="jumbotron text-center">
-  <h1>Obsługa warsztatów blacharskich</h1>
-  <p>Panel logowania</p>
- </div>
-<form class="box" action="login.php" method="post" autocomplete="off" >
 
-<h1> Zaloguj się </h1>
-<div id="inputs">
-  <br><input type="text" name="login" placeholder="Login"/> <br>
-  <br><input type="password" name="password" placeholder="Hasło"/><br>
+<div class="container">
+        <div class="subCont">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="leftCont"> 
+                        <form class="myForm text-center" action="login.php" method="post" autocomplete="off">
+                            <header>Logowanie</header>
+                            <div class="form-group">
+                                <i class="fas fa-user"></i>
+                                <input class="myInput" type="text" placeholder="Login" id="username" required> 
+                            </div>
+                            <div class="form-group">
+                                <i class="fas fa-lock"></i>
+                                <input class="myInput" type="password" id="password" placeholder="Hasło" required> 
+                            </div>
+                            <input type="submit" class="butt" value="ZALOGUJ">
+                            <div class="register-employee">
+                              <a href="index2.php">Rejestracja konta pracownika</a>
+                            </div>
+                        </form>
+                    </div>
+                </div> 
+                <div class="col-md-5">
+                    <div class="rightCont">
+                            <div class="box"><header>Panel logowania</header>
+                            
+                            <p>Zaloguj się danymi pracownika podanymi w procesie rejestracji. Jeśli nie jesteś jeszcze zarejestrowany użyj opcji "Rejestracja konta pracownika".</p>
+                                <input type="button" class="butt_out" value="Więcej informacji"/>
+                                
+                            </div>
+                                
+                    </div>
+                </div>
+            </div>
+        </div>
 </div>
-  <p><a href="index2.php">Tworzenie konta pracownika</a></p>
-<button type="submit" class="btn btn-outline-primary btn-lg" value="Zaloguj">Zaloguj</button>
-</form>
+      
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+
+
 </body>
 </html>
--- index2.php
+++ index2.php
@@ -4,16 +4,18 @@
 <head>
 
 
-<link rel="stylesheet" href="./css/bootstrap.min.css">
-<script src="./js/bootstrap.min.js"></script>
+<!-- <link rel="stylesheet" href="./css/bootstrap.min.css"> -->
+<!-- <script src="./js/bootstrap.min.js"></script> -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
 
   <title>Warsztaty Blacharskie</title>
 
  
 </head>
 <body>
-<div class="jumbotron text-center">
+<!-- <div class="jumbotron text-center">
   <h1>Obsługa warsztatów blacharskich</h1>
   <p>Panel rejestracji</p>
  </div>
@@ -27,7 +29,58 @@
 </div>
   <p><a href="index.php">Wróć do logowania</a></p>
 <button type="submit" class="btn btn-outline-primary btn-lg" value="Zarejestruj">Zarejestuj</button>
-</form>
+</form> -->
+
+
+<div class="container">
+        <div class="subCont">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="leftCont"> 
+                        <form class="myForm text-center" action="register.php" method="post" autocomplete="off">
+                            <header>Rejestracja</header>
+                            <div class="form-group">
+                                <i class="fas fa-key"></i>
+                                <input class="myInput" type="number" min="01010100000" max="99121299999" placeholder="PESEL" id="pesel" name="pesel" required> 
+                            </div>
+                            <div class="form-group">
+                                <i class="fas fa-user"></i>
+                                <input class="myInput" type="text" placeholder="Nazwa użytkownika" id="username"name="login" required> 
+                            </div>
+                            <div class="form-group">
+                                <i class="fas fa-lock"></i>
+                                <input class="myInput" type="password" id="password" placeholder="Hasło" name="password"  required> 
+                            </div>
+
+                            <div class="form-group">
+                                <label>
+                                    <input id="check_1" name="check_1"  type="checkbox" required><small> Przeczyałem i akceptuję regulamin</small></input> 
+                                    <div class="invalid-feedback">Zaznacz to pole jeśli chcesz kontynuować.</div>
+                                </label>
+                            </div>
+                            <input type="submit" class="butt" value="ZAREJESTRUJ SIĘ">
+                        </form>
+                    </div>
+                </div> 
+                <div class="col-md-5">
+                    <div class="rightCont">
+                            <div class="box"><header>Rejestracja</header>
+                            
+                            <p>Stwórz konto pracownika wpisując numer PESEL, nazwę użytkownika oraz hasło. Jeśli posiadasz już konto przejdź do logowania</p>
+                                <a href="index.php"><input type="button" class="butt_out" value="Logowanie"/></a>
+                                
+                            </div>
+                                
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+      
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+
 
 </body>
 </html>
