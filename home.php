70889fd6cbe8332a5964744cedead4eb317287e9
cyziek <krzysztof112997@gmail.com>
2021-04-27 18:59:18 +0200

    Update home.php

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <h2 class="display-4 text-white">Strona główna</h2>
-  <p class="lead text-white mb-0">Build a fixed sidebar using Bootstrap 4 vertical navigation and media objects.</p> 
+  <p class="lead text-white mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> 
   <div class="separator"></div>
   <div class="row text-white">
     <div class="col-lg-7">
