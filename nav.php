8d11efb8136004bc865f2a062368ddbe65cefc87
cyziek <krzysztof112997@gmail.com>
2021-04-29 12:56:16 +0200

    added subpages

--- nav.php
+++ nav.php
@@ -15,31 +15,31 @@
 
   <ul class="nav flex-column bg-white mb-0">
     <li class="nav-item">
-      <a href="#" class="nav-link text-dark bg-light">
+      <a href="dashboard.php" class="nav-link text-dark bg-light">
                 <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
                 Strona Główna
             </a>
     </li>
     <li class="nav-item">
-      <a href="#" class="nav-link text-dark">
+      <a href="pracownicy.php" class="nav-link text-dark">
                 <i class="fa fa-users mr-3 text-primary fa-fw"></i>
                 Pracownicy
             </a>
     </li>
     <li class="nav-item">
-      <a href="#" class="nav-link text-dark">
+      <a href="uslugi.php" class="nav-link text-dark">
                 <i class="fa fa-cubes mr-3 text-primary fa-fw"></i>
                 Usługi
             </a>
     </li>
     <li class="nav-item">
-      <a href="#" class="nav-link text-dark">
+      <a href="galeria.php" class="nav-link text-dark">
                 <i class="fa fa-picture-o mr-3 text-primary fa-fw"></i>
                 Galeria
             </a>
     </li>
     <li class="nav-item">
-      <a href="#" class="nav-link text-dark">
+      <a href="warsztaty.php" class="nav-link text-dark">
       <i class="fa fa-wrench mr-3 text-primary fa-fw"></i>
                 Warsztaty
             </a>
