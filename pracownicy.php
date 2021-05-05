ea15a68b22d8cffe0dd41a77d35f5007dd9d7ed2
RoZ0o0 <62017852+RoZ0o0@users.noreply.github.com>
2021-05-05 15:11:11 +0200

    Podzielone

--- pracownicy.php
+++ pracownicy.php
@@ -10,13 +10,13 @@
 <html>
 <head>
     <title>Usługi warsztatowe</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="style_dashboard.css">
+    <link rel="stylesheet" href="pracownik.css">
     <?php include 'nav.php';?>
-
-    <h1> SEKCJA PRACOWNIKÓW </h1>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>    
     <script>$(function() { 
         $('#sidebarCollapse').on('click', function() {
@@ -32,163 +32,10 @@
     });
     </script>
 
-<style>
-        .table-responsive {
-            margin: 30px 0;
-        }
-        .table-wrapper {
-            min-width: 1000px;
-            background: #fff;
-            padding: 20px 25px;
-            border-radius: 3px;
-            box-shadow: 0 1px 1px rgba(0,0,0,.05);
-        }
-        .table-title {
-            padding-bottom: 15px;
-            background: #299be4;
-            color: #fff;
-            padding: 16px 30px;
-            margin: -20px -25px 10px;
-            border-radius: 3px 3px 0 0;
-        }
-        .table-title h2 {
-            margin: 5px 0 0;
-            font-size: 24px;
-        }
-        .table-title .btn {
-            color: #566787;
-            float: right;
-            font-size: 13px;
-            background: #fff;
-            border: none;
-            min-width: 50px;
-            border-radius: 2px;
-            border: none;
-            outline: none !important;
-            margin-left: 10px;
-        }
-        .table-title .btn:hover, .table-title .btn:focus {
-            color: #566787;
-            background: #f2f2f2;
-        }
-        .table-title .btn i {
-            float: left;
-            font-size: 21px;
-            margin-right: 5px;
-        }
-        .table-title .btn span {
-            float: left;
-            margin-top: 2px;
-        }
-        table.table tr th, table.table tr td {
-            border-color: #e9e9e9;
-            padding: 12px 15px;
-            vertical-align: middle;
-        }
-        table.table tr th:first-child {
-            width: 60px;
-        }
-        table.table tr th:last-child {
-            width: 100px;
-        }
-        table.table-striped tbody tr:nth-of-type(odd) {
-            background-color: #fcfcfc;
-        }
-        table.table-striped.table-hover tbody tr:hover {
-            background: #f5f5f5;
-        }
-        table.table th i {
-            font-size: 13px;
-            margin: 0 5px;
-            cursor: pointer;
-        }	
-        table.table td:last-child i {
-            opacity: 0.9;
-            font-size: 22px;
-            margin: 0 5px;
-        }
-        table.table td a {
-            font-weight: bold;
-            color: #566787;
-            display: inline-block;
-            text-decoration: none;
-        }
-        table.table td a:hover {
-            color: #2196F3;
-        }
-        table.table td a.settings {
-            color: #2196F3;
-        }
-        table.table td a.delete {
-            color: #F44336;
-        }
-        table.table td i {
-            font-size: 19px;
-        }
-        table.table .avatar {
-            border-radius: 50%;
-            vertical-align: middle;
-            margin-right: 10px;
-        }
-        .status {
-            font-size: 30px;
-            margin: 2px 2px 0 0;
-            display: inline-block;
-            vertical-align: middle;
-            line-height: 10px;
-        }
-        .text-success {
-            color: #10c469;
-        }
-        .text-info {
-            color: #62c9e8;
-        }
-        .text-warning {
-            color: #FFC107;
-        }
-        .text-danger {
-            color: #ff5b5b;
-        }
-        .pagination {
-            float: right;
-            margin: 0 0 5px;
-        }
-        .pagination li a {
-            border: none;
-            min-width: 30px;
-            min-height: 30px;
-            color: #999;
-            margin: 0 2px;
-            line-height: 30px;
-            border-radius: 2px !important;
-            text-align: center;
-            padding: 0 6px;
-        }
-        .pagination li a:hover {
-            color: #666;
-        }	
-        .pagination li.active a, .pagination li.active a.page-link {
-            background: #03A9F4;
-        }
-        .pagination li.active a:hover {        
-            background: #0397d6;
-        }
-        .pagination li.disabled i {
-            color: #ccc;
-        }
-        .pagination li i {
-            font-size: 16px;
-            padding-top: 6px
-        }
-        .hint-text {
-            float: left;
-            margin-top: 10px;
-            font-size: 13px;
-        }
-    </style>
 
 </head>
 <body>
+    <center><h2 class="display-4 text-white">Sekcja pracownicy</h2></center>
     <div class="container">
         <div class="table-responsive">
             <div class="table-wrapper">
@@ -199,7 +46,7 @@
                         </div>
                         <div class="col-xs-7">
                             <a href="#" class="btn btn-primary"><i class="material-icons">&#xE147;</i> <span>Dodaj Pracownika</span></a>
-                            <a href="#" class="btn btn-primary"><i class="material-icons">&#xE24D;</i> <span>Exportuj do Excela</span></a>						
+                            <a href="#" class="btn btn-primary"><i class="material-icons">&#xE24D;</i> <span>Exportuj do Excela</span></a>	
                         </div>
                     </div>
                 </div>
@@ -210,7 +57,7 @@
                             <th>Imię i nazwisko</th>						
                             <th>Data zatrudnienia</th>
                             <th>Stanowisko</th>
-                            <th>Status konta</th>
+                            <th>PESEL</th>
                             <th>Akcja</th>
                         </tr>
                     </thead>
@@ -220,6 +67,8 @@
 
                         $polaczenie = @new mysqli($host, $user, $password, $db);
                     
+                        $polaczenie->set_charset("utf8");
+
                         if($polaczenie->connect_errno!=0){
                             echo "Error: ".$polaczenie->connect_errno;
                         }else{
@@ -227,6 +76,7 @@
                             $licznik = 1;
                             if($result = @$polaczenie->query($sql)){
                                 $ilu = $result->num_rows;
+                                $_SESSION['ile'] = $ilu;
                                 if($ilu>0){
                                     while($row = $result->fetch_assoc()){
 
@@ -235,7 +85,7 @@
                                             echo "<td><a href='#'>".$row['imie']." ".$row['nazwisko']."</a></td>";
                                             echo "<td>".$row['data_zatrudnienia']."</td>";                  
                                             echo "<td>".$row['stanowisko']."</td>";
-                                            echo "<td><span class='status text-success'>&bull;</span> Aktywne</td>";
+                                            echo "<td>".$row['pesel']."</td>";
                                             echo "<td>";
                                                 echo "<a href='#' class='settings' title='Settings' data-toggle='tooltip'><i class='material-icons'>&#xE8B8;</i></a>";
                                                 echo "<a href='#' class='delete' title='Delete' data-toggle='tooltip'><i class='material-icons'>&#xE5C9;</i></a>";
@@ -251,7 +101,7 @@
                     </tbody>
                 </table>
                 <div class="clearfix">
-                    <div class="hint-text">Pokazano <b>3</b> / <b>3</b> wyników.</div>
+                    <div class="hint-text">Pokazano <b>3</b> / <b><?php echo $_SESSION['ile']; ?></b> wyników.</div>
                     <ul class="pagination">
                         <li class="page-item disabled"><a href="#">Poprzedni</a></li>
                         <li class="page-item"><a href="#" class="page-link">1</a></li>
