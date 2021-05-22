11c7940b0d98cd3f6e3659c621c23da6cd4bca9a
RoZ0o0 <62017852+RoZ0o0@users.noreply.github.com>
2021-05-22 20:59:19 +0200

    Czesci gotowe

--- czesci.php
+++ czesci.php
@@ -1,36 +1,274 @@
 <?php
-  session_start();
-  $page = "czesci";
-  if(!isset($_SESSION['zalogowany'])){
-    header('Location: index.php');
-    exit();
-  }
+session_start();
+$page = "czesci";
+if (isset($_SESSION['komunikat'])) {
+  $blad = $_SESSION['komunikat'];
+}
+if (!isset($_SESSION['zalogowany'])) {
+  header('Location: index.php');
+  exit();
+}
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Usługi warsztatowe</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="style_dashboard.css">
+  <title>Usługi warsztatowe</title>
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="style_dashboard.css">
+  <link rel="stylesheet" href="pracownik.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+  <?php include 'nav.php'; ?>
+  <script>
+    $(function() {
+      $('#sidebarCollapse').on('click', function() {
+        $('#sidebar, #content').toggleClass('active');
+      });
+    });
+  </script>
+
+  <script>
+    $(document).ready(function() {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+  </script>
+
+
 </head>
+
 <body>
+  <center>
+    <h2 class="display-4 text-white">Sekcja części</h2>
+  </center>
+  <div class="container">
+    <div class="table-responsive">
+      <div class="table-wrapper">
+        <div class="table-title">
+          <div class="row">
+            <div class="col-xs-5">
+              <h2>Zarządzenie częściami</h2>
+            </div>
+            <div class="col-xs-2 ml-auto">
+              <input class="form-control" type="text" id="myInput" onkeyup="myFunction()" placeholder="Wyszukaj części">
+            </div>
+          </div>
+        </div>
+        <table id="table_to_highlight" class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nazwa</th>
+              <th>Numer</th>
+              <th>Opis</th>
+              <th>Cena</th>
+              <th>Sztuki</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            require_once "connect.php";
 
-<?php include 'nav.php';?>
-<center><h2 class="display-4 text-white">Sekcja Części</h2></center>
+            $polaczenie = oci_connect($user, $password, $db, 'AL32UTF8');
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<script src="main.js"></script>
-<script>$(function() { 
-  $('#sidebarCollapse').on('click', function() {
-    $('#sidebar, #content').toggleClass('active');
-  });
-});
+            // $polaczenie->set_charset("utf8");
 
-</script>
+            if (!$polaczenie) {
+              die("Connection failed: " . oci_error());
+            } else {
+              $stid = oci_parse($polaczenie, "SELECT * FROM czesci");
+              $licznik = 1;
+              if (oci_execute($stid) == TRUE) {
+                // $ilu = $result->num_rows;
+                // $_SESSION['ile'] = $ilu;
+                // if ($ilu > 0) {
+                while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+                  echo "<tr>";
+                  echo "<td>" . $licznik . "</td>";
+                  echo "<td>" . $row['NAZWA_CZESCI'] . "</td>";
+                  echo "<td>" . $row['NR_CZESCI'] . "</td>";
+                  echo "<td>" . $row['OPIS'] . "</td>";
+                  echo "<td>" . $row['CENA'] . "</td>";
+                  echo "<td>" . $row['LICZBA_DOSTEPNYCH_SZTUK'] . "</td>";
+                  echo "</tr>";
+                  $licznik++;
+                }
+              }
+            }
+            oci_close($polaczenie);
+            ?>
+          </tbody>
+        </table>
+
+        <div class='pagination-container'>
+          <nav>
+            <ul class="pagination">
+
+              <li data-page="prev">
+                <span>
+                  < <span class="sr-only">(current)
+                </span></span>
+              </li>
+              <li data-page="next" id="prev">
+                <span> > <span class="sr-only">(current)</span></span>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="form-group hidden">
+          <select class="form-control" name="state" id="maxRows" style="width: 10%">
+            <option value="5000">Wszystko</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+    getPagination('#table_to_highlight');
+
+    function getPagination(table) {
+      var lastPage = 1;
+
+      $('#maxRows')
+        .on('change', function(evt) {
+
+          lastPage = 1;
+          $('.pagination')
+            .find('li')
+            .slice(1, -1)
+            .remove();
+          var trnum = 0;
+          var maxRows = parseInt($(this).val());
+
+          if (maxRows == 5000) {
+            $('.pagination').hide();
+          } else {
+            $('.pagination').show();
+          }
+
+          if (maxRows)
+
+            var totalRows = $(table + ' tbody tr').length;
+          $(table + ' tr:gt(0)').each(function() {
+            trnum++;
+            if (trnum > maxRows) {
+
+              $(this).hide();
+            }
+            if (trnum <= maxRows) {
+              $(this).show();
+            }
+          });
+          if (totalRows > maxRows) {
+            var pagenum = Math.ceil(totalRows / maxRows);
+            for (var i = 1; i <= pagenum;) {
+              $('.pagination #prev')
+                .before(
+                  '<li data-page="' +
+                  i +
+                  '">\
+								  <span>' +
+                  i++ +
+                  '<span class="sr-only">(current)</span></span>\
+								</li>'
+                )
+                .show();
+            }
+          }
+          $('.pagination [data-page="1"]').addClass('active');
+          $('.pagination li').on('click', function(evt) {
+            evt.stopImmediatePropagation();
+            evt.preventDefault();
+            var pageNum = $(this).attr('data-page');
+
+            var maxRows = parseInt($('#maxRows').val());
+
+            if (pageNum == 'prev') {
+              if (lastPage == 1) {
+                return;
+              }
+              pageNum = --lastPage;
+            }
+            if (pageNum == 'next') {
+              if (lastPage == $('.pagination li').length - 2) {
+                return;
+              }
+              pageNum = ++lastPage;
+            }
+
+            lastPage = pageNum;
+            var trIndex = 0;
+            $('.pagination li').removeClass('active');
+            $('.pagination [data-page="' + lastPage + '"]').addClass('active');
+            limitPagging();
+            $(table + ' tr:gt(0)').each(function() {
+              trIndex++;
+              if (
+                trIndex > maxRows * pageNum ||
+                trIndex <= maxRows * pageNum - maxRows
+              ) {
+                $(this).hide();
+              } else {
+                $(this).show();
+              }
+            });
+          });
+          limitPagging();
+        })
+        .val(5)
+        .change();
+    }
+
+    function limitPagging() {
+
+      if ($('.pagination li').length > 7) {
+        if ($('.pagination li.active').attr('data-page') <= 3) {
+          $('.pagination li:gt(5)').hide();
+          $('.pagination li:lt(5)').show();
+          $('.pagination [data-page="next"]').show();
+        }
+        if ($('.pagination li.active').attr('data-page') > 3) {
+          $('.pagination li:gt(0)').hide();
+          $('.pagination [data-page="next"]').show();
+          for (let i = (parseInt($('.pagination li.active').attr('data-page')) - 2); i <= (parseInt($('.pagination li.active').attr('data-page')) + 2); i++) {
+            $('.pagination [data-page="' + i + '"]').show();
+
+          }
+
+        }
+      }
+    }
+
+    $(document).ready(function() {
+      $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#table_to_highlight tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    });
+  </script>
+  <script>
+    $(document).ready(function() {
+      $('#myInput').on("keyup",function() {
+        if (!$(this).val()) {
+          $('#maxRows').val(5).change();
+        }
+      });
+    });
+  </script>
 </body>
-</html>
 
+</html>
