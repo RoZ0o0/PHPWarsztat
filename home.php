e8730530d9b96a2b0764d2f0d1f74789834e97a9
RoZ0o0 <62017852+RoZ0o0@users.noreply.github.com>
2021-05-26 14:24:28 +0200

    Warsztaty TOP 5

--- home.php
+++ home.php
@@ -16,6 +16,7 @@
         <option value="4">Kody pocztowe</option>
         <option value="5">Części</option>
         <option value="6">TOP 5 Pracowników</option>
+        <option value="7">TOP 5 Warsztatów</option>
       </select>
     </div>
     <div id="container" class="container" style="position: relative; left: 20%;">
@@ -29,13 +30,17 @@
         <div id="naprawy" style="width: 970px; height: 550px;padding-top: 25px;padding-left: 60px; margin-top:10px;"></div>
       </div>
       <div class="outside-chart" id="div4" style="width: 1110px;height:570px;background-color:#fff; margin-left:auto; margin-right:auto;border-radius:20px;">
-      <div id="klienci_kody" style="width: 970px; height: 570px;margin-left:auto;margin-right:auto;margin-top:10px;"></div></div>
+        <div id="klienci_kody" style="width: 970px; height: 570px;margin-left:auto;margin-right:auto;margin-top:10px;"></div>
+      </div>
 
       <div class="outside-chart" id="div5" style="width: 1110px;height:570px;background-color:#ffffff; margin-left:auto; margin-right:auto;border-radius:20px;">
-       <div id="czesci_wykres" style="width: 970px; height: 550px;padding-top: 25px;padding-left: 60px; margin-top:10px;"></div>
+        <div id="czesci_wykres" style="width: 970px; height: 550px;padding-top: 25px;padding-left: 60px; margin-top:10px;"></div>
       </div>
       <div class="outside_chart" id="div6" style="width: 1110px;height:570px;background-color:#ffffff; margin-left:auto; margin-right:auto;border-radius:20px;">
         <div id="pracownicy_top" style="width: 970px; height: 550px;padding-top: 25px;padding-left: 60px; margin-top:10px;"></div>
+      </div>
+      <div class="outside_chart" id="div7" style="width: 1110px;height:570px;background-color:#ffffff; margin-left:auto; margin-right:auto;border-radius:20px;">
+        <div id="warsztaty_top" style="width: 970px; height: 550px;padding-top: 25px;padding-left: 60px; margin-top:10px;"></div>
       </div>
     </div>
 
@@ -212,88 +217,96 @@
       };
     </script>
 
-<!-- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> -->
-  <script type="text/javascript">
-    google.charts.load("current", {
-      packages: ["corechart"]
-    });
-    google.charts.setOnLoadCallback(drawKlienciKody);
+    <!-- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> -->
+    <script type="text/javascript">
+      google.charts.load("current", {
+        packages: ["corechart"]
+      });
+      google.charts.setOnLoadCallback(drawKlienciKody);
 
-    function drawKlienciKody() {
-      var data = google.visualization.arrayToDataTable([
-        ['Kod pocztowy', 'Częstotliwość'],
-        <?php
-        require_once "connect.php";
-        $polaczenie = oci_connect($user, $password, $db, 'AL32UTF8');
+      function drawKlienciKody() {
+        var data = google.visualization.arrayToDataTable([
+          ['Kod pocztowy', 'Częstotliwość'],
+          <?php
+          require_once "connect.php";
+          $polaczenie = oci_connect($user, $password, $db, 'AL32UTF8');
 
-        // $polaczenie->set_charset("utf8");
+          // $polaczenie->set_charset("utf8");
 
-        if (!$polaczenie) {
-          die("Connection failed: " . oci_error());
-        } else {
-          $curs = oci_new_cursor($polaczenie);
-          $stid = oci_parse($polaczenie, "BEGIN STATYSTYKI.KLIENCI_KODY(:cursr); END;");
-          oci_bind_by_name($stid, ":cursr", $curs, -1, OCI_B_CURSOR);
-          oci_execute($stid);
-          oci_execute($curs);
+          if (!$polaczenie) {
+            die("Connection failed: " . oci_error());
+          } else {
+            $curs = oci_new_cursor($polaczenie);
+            $stid = oci_parse($polaczenie, "BEGIN STATYSTYKI.KLIENCI_KODY(:cursr); END;");
+            oci_bind_by_name($stid, ":cursr", $curs, -1, OCI_B_CURSOR);
+            oci_execute($stid);
+            oci_execute($curs);
 
-          while (($row = oci_fetch_array($curs, OCI_BOTH)) != false) {
-            echo "['" . $row['KOD_POCZTOWY'] . "'," . $row['KOD'] . "],";
+            while (($row = oci_fetch_array($curs, OCI_BOTH)) != false) {
+              echo "['" . $row['KOD_POCZTOWY'] . "'," . $row['KOD'] . "],";
+            }
           }
-        }
-        oci_close($polaczenie);
-        ?>
-      ]);
+          oci_close($polaczenie);
+          ?>
+        ]);
 
 
 
-      var options = {
-        title: 'Kody pocztowe według ilości klientów',
-        is3D: true,
-        fontSize: 20,
-        sliceVisibilityThreshold: .1,
-        slices: {
-            0: { color: '#2B987E' },
-            1: { color: '#BDBA04' },
-            2: { color: '#A1C6CA'},
+        var options = {
+          title: 'Kody pocztowe według ilości klientów',
+          is3D: true,
+          fontSize: 20,
+          sliceVisibilityThreshold: .1,
+          slices: {
+            0: {
+              color: '#2B987E'
+            },
+            1: {
+              color: '#BDBA04'
+            },
+            2: {
+              color: '#A1C6CA'
+            },
           }
         };
 
-      var chart = new google.visualization.PieChart(document.getElementById('klienci_kody'));
-      chart.draw(data, options);
-    }
-  </script>
+        var chart = new google.visualization.PieChart(document.getElementById('klienci_kody'));
+        chart.draw(data, options);
+      }
+    </script>
 
 
 
-<script>
-      google.charts.load('current', {'packages':['bar']});
+    <script>
+      google.charts.load('current', {
+        'packages': ['bar']
+      });
       google.charts.setOnLoadCallback(czesci_wykres);
 
       function czesci_wykres() {
         var data = new google.visualization.arrayToDataTable([
           ['Nazwa części', 'Ilość'],
           <?php
-        require_once "connect.php";
-        $polaczenie = oci_connect($user, $password, $db, 'AL32UTF8');
+          require_once "connect.php";
+          $polaczenie = oci_connect($user, $password, $db, 'AL32UTF8');
 
-        // $polaczenie->set_charset("utf8");
+          // $polaczenie->set_charset("utf8");
 
-        if (!$polaczenie) {
-          die("Connection failed: " . oci_error());
-        } else {
-          $curs = oci_new_cursor($polaczenie);
-          $stid = oci_parse($polaczenie, "BEGIN STATYSTYKI.CZESCI_WYKRES(:cursr); END;");
-          oci_bind_by_name($stid, ":cursr", $curs, -1, OCI_B_CURSOR);
-          oci_execute($stid);
-          oci_execute($curs);
+          if (!$polaczenie) {
+            die("Connection failed: " . oci_error());
+          } else {
+            $curs = oci_new_cursor($polaczenie);
+            $stid = oci_parse($polaczenie, "BEGIN STATYSTYKI.CZESCI_WYKRES(:cursr); END;");
+            oci_bind_by_name($stid, ":cursr", $curs, -1, OCI_B_CURSOR);
+            oci_execute($stid);
+            oci_execute($curs);
 
-          while (($row = oci_fetch_array($curs, OCI_BOTH)) != false) {
-            echo "['" . $row['NAZWA_CZESCI'] . "'," . $row['ILE'] . "],";
+            while (($row = oci_fetch_array($curs, OCI_BOTH)) != false) {
+              echo "['" . $row['NAZWA_CZESCI'] . "'," . $row['ILE'] . "],";
+            }
           }
-        }
-        oci_close($polaczenie);
-        ?>
+          oci_close($polaczenie);
+          ?>
         ]);
 
         var options = {
@@ -304,7 +317,7 @@
           legend: {
             position: 'none'
           },
-    
+
           chart: {
             title: 'Najczęściej sprzedawane części',
             subtitle: ''
@@ -326,12 +339,10 @@
         var chart = new google.charts.Bar(document.getElementById('czesci_wykres'));
         chart.draw(data, google.charts.Bar.convertOptions(options));
       };
+    </script>
 
 
-</script>
-
-
-<script>
+    <script>
       google.charts.load('current', {
         'packages': ['bar']
       });
@@ -372,7 +383,7 @@
           legend: {
             position: 'none'
           },
-    
+
           chart: {
             title: 'TOP 5 Pracowników',
             subtitle: ''
@@ -397,6 +408,71 @@
       };
     </script>
 
+<script>
+      google.charts.load('current', {
+        'packages': ['bar']
+      });
+      google.charts.setOnLoadCallback(warsztaty_top);
+
+      function warsztaty_top() {
+        var data = new google.visualization.arrayToDataTable([
+          ['Warsztat', 'Usługi'],
+          <?php
+          require_once "connect.php";
+
+          $polaczenie = oci_connect($user, $password, $db, 'AL32UTF8');
+
+          // $polaczenie->set_charset("utf8");
+
+          if (!$polaczenie) {
+            die("Connection failed: " . oci_error());
+          } else {
+            $curs = oci_new_cursor($polaczenie);
+            $stid = oci_parse($polaczenie, "BEGIN STATYSTYKI.WARSZTATY_TOP(:cursr); END;");
+            oci_bind_by_name($stid, ":cursr", $curs, -1, OCI_B_CURSOR);
+            oci_execute($stid);
+            oci_execute($curs);
+
+            while (($row = oci_fetch_array($curs, OCI_BOTH)) != false) {
+              echo "['" . $row['WARSZTAT'] . "'," . $row['ILE'] . "],";
+            }
+          }
+          oci_close($polaczenie);
+          ?>
+        ]);
+
+        var options = {
+          width: 970,
+          colors: '#066997',
+          fontSize: 20,
+          bars: 'horizontal',
+          legend: {
+            position: 'none'
+          },
+    
+          chart: {
+            title: 'TOP 5 Warsztatów',
+            subtitle: ''
+          },
+          axes: {
+            x: {
+              0: {
+                side: 'top',
+                label: ''
+              } // Top x-axis.
+            }
+          },
+          bar: {
+            groupWidth: "90%"
+          }
+        };
+
+
+        var chart = new google.charts.Bar(document.getElementById('warsztaty_top'));
+        // Convert the Classic options to Material options.
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      };
+    </script>
 
     <script>
       $(".chartSelect").change(function() {
@@ -406,12 +482,14 @@
           $("#div3").show();
           $("#div4").show();
           $("#div5").show();
+          $("#div6").show();
+          $("#div7").show();
         } else {
           $("#div" + $(this).val()).show().siblings().hide();
         }
       });
     </script>
-    
+
   </div>
 </div>
 </div>
